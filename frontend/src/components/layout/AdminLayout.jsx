7fc598c87f4afd04315f9d29afa44ca34988b7c7
Bisu56 <rajbanshibishal441@gmail.com>
2026-02-18 22:37:05 +0545

    AdminLayout

---
+++ frontend/src/components/layout/AdminLayout.jsx
@@ -0,0 +1,11 @@
+import Sidebar from './Sidebar';
+
+const AdminLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 p-6 bg-gray-100">{children}</div>
+    </div>
+  );
+}
+export default AdminLayout
