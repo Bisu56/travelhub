b288ace8691864823f841a4590ad8281c3e9b441
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-02-23 22:54:20 +0545

    feat (Payment controller impl)

---
+++ backend/src/main/java/com/travelhub/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.travelhub.controller;
+
+import com.travelhub.Dtos.PaymentInitiateResponseDTO;
+import com.travelhub.entity.User;
+import com.travelhub.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/{cartId}")
+    public PaymentInitiateResponseDTO initiate(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long cartId,
+            @RequestParam String gateway
+    ) throws Exception {
+
+        return paymentService.initiatePayment(user, cartId, gateway);
+    }
+}
