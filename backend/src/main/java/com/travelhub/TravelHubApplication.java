f73bfc095ec686f0ea08db076412da934e88eb37
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-02-27 21:12:43 +0545

    feat(Setting Spring Boot default timezone to UTC)

--- backend/src/main/java/com/travelhub/TravelHubApplication.java
+++ backend/src/main/java/com/travelhub/TravelHubApplication.java
@@ -1,12 +1,19 @@
 package com.travelhub;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class TravelHubApplication {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TravelHubApplication.class, args);
